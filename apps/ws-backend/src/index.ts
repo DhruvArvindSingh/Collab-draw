d5538971fe604ab5b3b079aaae6b6c05acd22f81
DhruvArvindSingh <dhruvsinghxd@gmail.com>
2025-02-20 05:12:05 +0530

    Needs work in chat endpoint

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,34 +1,97 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
 
-const { JWT_SECRET }= process.env;
 
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new WebSocketServer({ port: 8081 });
+const { JWT_SECRET } = process.env;
+interface User {
+    ws: WebSocket;
+    user_id: number;
+    room_id: string[];
+}
 
-wss.on("connection", (ws, request) => {
+let users: User[] = [];
+
+
+// Create WebSocket server attached to the HTTP server
+
+function verify_token(token: string): string | null {
+    const decoded = jwt.verify(token, JWT_SECRET as string);
+    console.log("decoded =", decoded);
+    if (typeof decoded === "string") {
+        return null;
+    }
+    if (!decoded || !decoded.userId) {
+        return null;
+    }
+    return decoded.userId;
+}
+
+wss.on("connection", function connection(ws, request){
     console.log("Client connected");
     const url = request.url;
-    
-    if(!url){
+    if (!url) {
         return;
     }
-    const queryParams = new URLSearchParams(url.split("?")[1]);
-    const token = queryParams.get("token") || "";
-    const decoded = jwt.verify(token, JWT_SECRET as string);
-    if(typeof decoded === "string"){
+    try{
+        const queryParams = new URLSearchParams(url.split("?")[1]);
+        const token = queryParams.get("token") || "";
+        // console.log("token =", token);
+        const user_id = verify_token(token);
+        console.log("user_id =", user_id);
+        // console.log("typeof users =", typeof user_id);
+        if (!user_id) {
+            ws.close();
+            return;
+        }
+        users.push({
+            ws: ws,
+            user_id: parseInt(user_id),
+            room_id: []
+        });
+        console.log("users =", users);
+    }
+    catch(error){
         ws.close();
+        console.log("Unauthorized user with no token");
         return;
     }
-    if(!decoded || !decoded.user_id){
-        ws.close();
-        return;
-    }
-    console.log("room_id =", room_id);
-    
+
 
     ws.on("message", (message) => {
-        console.log(message);
+        const data = JSON.parse(message.toString());
+        if (data.type === "join_room") {
+            const user = users.find((user) => user.ws === ws);
+            if(user?.room_id.includes(data.room_id)){
+                console.log("user already in room");
+                return;
+            }
+            user?.room_id.push(data.room_id);
+            console.log("join_room user =", user);
+        }
+        if (data.type === "leave_room") {
+            const user = users.find((user) => user.ws === ws);
+            if (user) {
+                if(!user?.room_id.includes(data.room_id)){
+                    console.log("user not in room");
+                    return;
+                }
+                user.room_id = user.room_id.filter((room) => room !== data.room_id);
+                console.log("leave_room user =", user);
+            } else {
+                console.log("user not found");
+                return;
+            }
+        }
+        if (data.type === "chat") {
+            users.forEach((user) => {
+                if (user.room_id.includes(data.room_id)) {
+                    user.ws.send(JSON.stringify({ type: "chat", message: data.message, room_id: data.room_id }));
+                }
+            });
+        }
     });
 });
+
